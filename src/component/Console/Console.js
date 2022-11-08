6de34f71bdcaf8b8bc29d54e49b7cad4de7a0959
ogzCode <oguzguc44@gmail.com>
2022-11-08 07:55:27 +0300

    Bug fixed

--- src/component/Console/Console.js
+++ src/component/Console/Console.js
@@ -2,70 +2,10 @@ import React from "react";
 import "./Console.css";
 import tokenizeForPrint from "./Function/tokenizeForPrint";
 import isVariable from "./Function/isVariable";
+import tokenizeForOperation from "./Function/tokenizeForOperation";
 
-function tokenizeForOperation(operation, variableList){
-    const op = tokenizeProsses(operation.prosses);
 
-    let var1 = isVariable(op.variableOne, variableList);
-    let var2 = isVariable(op.variableTwo, variableList);
-    let result = isVariable(operation.result, variableList);
-    let res = null;
-
-    if (var1 && var2){
-        if (op.operator === "+"){
-            res = var1.value + var2.value;
-        }
-        else if (op.operator === "-"){
-            res = var1.value - var2.value;
-        }
-        else if (op.operator === "*"){
-            res = var1.value * var2.value;
-        }
-        else if (op.operator === "/"){
-            res = var1.value / var2.value;
-        }
-    }
-    else {
-        return false;
-    }
-
-    if (result){
-        let index = variableList.indexOf(result);
-        variableList[index].value = res;
-    }
-    else {
-        return false;
-    }
-}
-
-function tokenizeProsses(data){
-    let operation = {
-        "variableOne": "",
-        "operator": "",
-        "variableTwo": ""
-    };
-    let charList = [];
-
-    for (let char of data){
-        if ("+-*/".includes(char)){
-            if (operation.variableOne === ""){
-                operation.variableOne = charList.join("");
-                charList = charList.splice();
-                operation.operator = char;
-            }
-            else {
-                return false;
-            }
-        }
-        else {
-            charList.push(char);
-        }
-    }
-    operation.variableTwo = charList.join("");
-    return operation;
-}
-
-function tokenizeExpression(value){
+function tokenizeExpression(value) {
     const operation = {
         "valueOne": "",
         "operator": "",
@@ -74,9 +14,9 @@ function tokenizeExpression(value){
 
     let charList = [];
 
-    for (let char of value){
-        if ("><=!".includes(char)){
-            if (operation.valueOne === ""){
+    for (let char of value) {
+        if ("><=!".includes(char)) {
+            if (operation.valueOne === "") {
                 operation.valueOne = charList.join("");
                 charList = charList.splice();
                 operation.operator = char;
@@ -93,25 +33,25 @@ function tokenizeExpression(value){
     return operation;
 }
 
-function tokenizeForCondition(data, variableList){
+function tokenizeForCondition(data, variableList) {
     const op = tokenizeExpression(data.control);
 
     let var1 = isVariable(op.valueOne, variableList);
     let var2 = isVariable(op.valueTwo, variableList);
     let res = null;
 
-    if (var1 && var2){
-        if (op.operator === ">"){
+    if (var1 && var2) {
+        if (op.operator === ">") {
             res = var1.value > var2.value;
         }
-        else if (op.operator === "<"){
+        else if (op.operator === "<") {
             res = var1.value < var2.value;
         }
-        else if (op.operator === "="){
-            res = var1.value == var2.value;
+        else if (op.operator === "=") {
+            res = var1.value === var2.value;
         }
-        else if (op.operator === "!"){
-            res = var1.value != var2.value;
+        else if (op.operator === "!") {
+            res = var1.value !== var2.value;
         }
     }
     else {
@@ -120,7 +60,7 @@ function tokenizeForCondition(data, variableList){
 
     let text = null;
 
-    if (res){
+    if (res) {
         text = tokenizeForPrint(data.isTrue.split(" "), variableList);
     }
     else {
@@ -131,53 +71,53 @@ function tokenizeForCondition(data, variableList){
 }
 
 class Console extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.handleCloseBtn = this.handleCloseBtn.bind(this);
         this.state = {
             variableList: [],
             printTextList: []
-        }; 
+        };
     }
-    handleCloseBtn(){
+    handleCloseBtn() {
         this.props.onClick(false);
     }
-    componentDidMount(){
+    componentDidMount() {
         this.compiler();
     }
-    componentDidUpdate(prevProps){
-        if (this.props.data.lenght > prevProps.data.lenght){
+    componentDidUpdate(prevProps) {
+        if (this.props.data.lenght > prevProps.data.lenght) {
             this.compiler();
         }
-        else if (this.props.data.length < prevProps.data.length){
+        else if (this.props.data.length < prevProps.data.length) {
             this.setState({
                 variableList: [],
                 printTextList: []
             });
         }
     }
-    compiler(){
+    compiler() {
         const variableList = this.state.variableList.slice();
         const printTextList = this.state.printTextList.slice();
-        for (let d of this.props.data){
-            if (d.type === "variable"){
-                variableList.push({"name": d.name, "value": d.value});
+        for (let d of this.props.data) {
+            if (d.type === "variable") {
+                variableList.push({ "name": d.name, "value": d.value });
             }
-            else if (d.type === "print"){
+            else if (d.type === "print") {
                 let res = tokenizeForPrint(d.value.split(" "), variableList);
                 printTextList.push(res);
             }
-            else if (d.type === "operation"){
+            else if (d.type === "operation") {
                 let res = tokenizeForOperation(d, variableList);
 
-                if(res === false){
+                if (res === false) {
                     printTextList.push("Operation not compiled");
                 }
             }
-            else if (d.type === "statement"){
+            else if (d.type === "statement") {
                 let res = tokenizeForCondition(d, variableList);
 
-                if (res === false){
+                if (res === false) {
                     printTextList.push("Condition not compiled");
                 }
                 else {
@@ -191,7 +131,7 @@ class Console extends React.Component {
             printTextList: printTextList
         });
     }
-    render(){
+    render() {
         return (
             <div className="console">
                 <button className="close__btn" onClick={this.handleCloseBtn}>Close</button>
---
+++ src/component/Console/Function/tokenizeForOperation.js
@@ -0,0 +1,173 @@
+import isVariable from "./isVariable";
+
+function prec(x) {
+    if (x === "+" || x === "-") {
+        return 1;
+    }
+    else if (x === "*" || x === "/") {
+        return 2;
+    }
+    else {
+        return -1;
+    }
+}
+
+function infixToPostfix(state) {
+    let stack = [];
+    let result = "";
+    let resultList = [];
+
+    for (let i = 0; i < state.length; i++) {
+        let c = state[i];
+
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+            result += c;
+        }
+        else {
+            resultList.push(result);
+            result = "";
+            while (stack.length !== 0 && prec(c) <= prec(stack[stack.length - 1])) {
+                resultList.push(stack[stack.length - 1]);
+                stack.pop();
+            }
+            stack.push(c);
+        }
+    }
+    resultList.push(result);
+    while (stack.length !== 0) {
+        resultList.push(stack[stack.length - 1]);
+        stack.pop();
+    }
+    console.log(resultList);
+
+    return resultList;
+}
+
+function calcExpression(ex, variableList) {
+    let stack = [];
+
+    for (let i of ex) {
+        let n = parseInt(i);
+        if (n) {
+            stack.push(n);
+        }
+        else if ("+-*/".includes(i)) {
+            let numberOne = stack.pop();
+            let numberTwo = stack.pop();
+
+            if (i === "+") {
+                stack.push(numberOne + numberTwo);
+            }
+            else if (i === "-") {
+                stack.push(numberOne - numberTwo);
+            }
+            else if (i === "*") {
+                stack.push(numberOne * numberTwo);
+            }
+            else if (i === "/") {
+                stack.push(numberOne / numberTwo);
+            }
+        }
+        else {
+            let v = isVariable(i, variableList);
+
+            if (v) {
+                stack.push(v.value);
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    return stack[0];
+}
+
+function tokenizeForOperation(operation, variableList){
+    let post = infixToPostfix(operation.prosses);
+    let calc = calcExpression(post, variableList);
+    let result = isVariable(operation.result, variableList);
+
+    if (!calc){
+        return false;
+    }
+
+    if (result) {
+        let index = variableList.indexOf(result);
+        variableList[index].value = calc;
+    }
+    else {
+        return false;
+    }
+}
+
+/*
+function tokenizeForOperation(operation, variableList) {
+    //const op = tokenizeProsses(operation.prosses);
+
+    let post = infixToPostfix(operation.prosses);
+    let calc = calcExpression(post, variableList);
+
+    if (!calc){
+        return false;
+    }
+
+    //let var1 = isVariable(op.variableOne, variableList);
+    //let var2 = isVariable(op.variableTwo, variableList);
+    let result = isVariable(operation.result, variableList);
+
+    if (var1 && var2){
+        if (op.operator === "+"){
+            res = var1.value + var2.value;
+        }
+        else if (op.operator === "-"){
+            res = var1.value - var2.value;
+        }
+        else if (op.operator === "*"){
+            res = var1.value * var2.value;
+        }
+        else if (op.operator === "/"){
+            res = var1.value / var2.value;
+        }
+    }
+    else {
+        return false;
+    } 
+
+    if (result) {
+        let index = variableList.indexOf(result);
+        variableList[index].value = calc;
+    }
+    else {
+        return false;
+    }
+}
+
+function tokenizeProsses(data) {
+    let operation = {
+        'variableOne': '',
+        'operator': '',
+        'variableTwo': ''
+    };
+    let charList = [];
+
+    for (let char of data) {
+        if ('+-/*'.includes(char)) {
+            if (operation.variableOne === '') {
+                operation.variableOne = charList.join('');
+                charList = charList.splice();
+                operation.operator = char;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            charList.push(char);
+        }
+    }
+    operation.variableTwo = charList.join('');
+    return operation;
+}
+ */
+
+export default tokenizeForOperation;
